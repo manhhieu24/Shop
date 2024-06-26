c7454c4240e3af1f967bebcd862d0fb906e025a1
manhhieu24 <manhhieu24@gmail.com>
2024-06-26 22:43:08 +0700

    add static_root

--- demo/settings.py
+++ demo/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR / "app/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
